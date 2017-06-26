c13e2d85b9dab88128134924a45869543e2afc63
Willot <vwillot@deloitte.com>
2017-06-26 14:42:32 -0400

    manage to test intent on groceries button

--- app/src/androidTest/java/com/grocerieslist/grocerieslist/MainActivityTest.java
+++ app/src/androidTest/java/com/grocerieslist/grocerieslist/MainActivityTest.java
@@ -1,8 +1,13 @@
 package com.grocerieslist.grocerieslist;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
@@ -13,6 +18,12 @@ import org.junit.Test;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -35,7 +46,22 @@ public class MainActivityTest{
     }
 
     @Test
-    public void testGorcerieButtonBackgroundChangeColor() throws Exception {
+    public void testGorcerieButtonClickChangeText() throws Exception {
         onView(withId(R.id.groceries)).perform(click()).check(matches(withText("You clicked it")));
     }
+
+
+    @Test
+    public void testGoToGroceriesActivityAfterClickingGrocerieButon() throws Exception {
+
+        Intents.init();
+
+        onView(withId(R.id.groceries)).check(matches(withText("Add Groceries")));
+            onView(withId(R.id.groceries)).perform(click());
+
+        intended(hasComponent(GroceryActivity.class.getName()));
+
+        Intents.release();
+    }
+
 }
--- app/src/main/java/com/grocerieslist/grocerieslist/MainActivity.java
+++ app/src/main/java/com/grocerieslist/grocerieslist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.grocerieslist.grocerieslist;
 
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickGroceriesButton(View view){
 //        mGroceriesButton.setBackgroundColor(ContextCompat.getColor(this,R.color.colorFuchsia));
+        Intent intent = new Intent(this, GroceryActivity.class);
+
         mGroceriesButton.setText("You clicked it");
+        startActivity(intent);
     }
 
 
